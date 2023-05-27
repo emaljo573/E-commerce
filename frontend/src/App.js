c6f322f7b9f074d05c3b211a959cdf8a90ce5cf6
Emal Johny <emaljo573@gmail.com>
2023-05-27 17:15:23 +0530

    Frontend changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,24 @@
-
-const App=()=> {
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from 'react-bootstrap'
+import HomeScreen from './screens/HomeScreen'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import ProductScreen from "./screens/ProductScreen";
+const App = () => {
   return (
-   <>
-   </>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} exact/>
+            <Route path="/product/:id" 
+            element={<ProductScreen/>}/>
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import products from '../products'
+import { Row, Col } from 'react-bootstrap'
+import Product from '../components/Product'
+
+const HomeScreen = () => {
+    return (
+        <>
+            <h1>Latest Products</h1>
+            <Row>
+                {products.map(el => (
+                    <Col key={el._id} sm={12} md={6} lg={4} xl={3}>
+                        <Product
+                            product={el} />
+                    </Col>
+                ))}
+            </Row>
+        </>
+    )
+}
+
+export default HomeScreen
